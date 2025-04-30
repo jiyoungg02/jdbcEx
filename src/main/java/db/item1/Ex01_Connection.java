af2510ca692144278ca750c186adf39605e7833a
jiyoungg02 <jiyoung_g02@naver.com>
2025-04-30 17:41:44 +0900

    0430 oracle/java 연결

---
+++ src/main/java/db/item1/Ex01_Connection.java
@@ -0,0 +1,13 @@
+package db.item1;
+
+import java.sql.Connection;
+
+import db.util.DBConn;
+
+public class Ex01_Connection {
+	public static void main(String[] args) {
+		Connection conn = DBConn.getConnection();
+		
+		System.out.println(conn);
+	}
+}
---
+++ src/main/java/db/item3/score/App.java
@@ -0,0 +1,7 @@
+package db.item3.score;
+
+public class App {
+	public static void main(String[] args) {
+		new ScoreUI().menu();
+	}
+}
